fc45f820bfdbb86f41ea19557e6f66be0c3cb94d
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2024-03-07 16:05:45 -0300

    Progress in models and seeders files

---
+++ app/backend/src/database/migrations/02.matches.migration.ts
@@ -0,0 +1,50 @@
+import { Model, QueryInterface, DataTypes } from 'sequelize';
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model>('matches', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      home_team_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      home_team_goals: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      away_team_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      away_team_goals: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      in_progress: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+    });
+  },
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('matches');
+  },
+};
